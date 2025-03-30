c2da8e6d384bea7f38138584c115b3286e6ea52e
Fabian Bartholdi <fafa.fsb@gmail.com>
2025-03-30 20:30:36 +0200

    first commit

---
+++ scripts/db.js
@@ -0,0 +1,27 @@
+let MyDishes = [
+    {
+        name: "Pizza Margherita",
+        description: "Tomatensoße mit frischen Tomaten, Mozzarellakäse und Basilikum",
+        price: 10.99,
+    },
+    {
+        name: "Burger",
+        description: "Saftiger Rindfleischburger",
+        price: 8.99,
+    },
+    {
+        name: "Pasta",
+        description: "Cremige Alfredo-Pasta",
+        price: 12.99,
+    },
+    {
+        name: "Salad",
+        description: "Frischer Gartensalat",
+        price: 7.99,
+    },
+    {
+        name: "Sushi-Platte",
+        description: "Verschiedene Sushi-Platten",
+        price: 15.99,
+    }
+]
